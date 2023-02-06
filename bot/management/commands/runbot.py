ca5a17cbfdd58c4cc018affd21061552f4d8dac9
VladmimirShtefan <shtefanvladimir14@gmail.com>
2023-02-06 22:42:51 +0700

    рефактор redis

--- bot/management/commands/runbot.py
+++ bot/management/commands/runbot.py
@@ -17,7 +17,6 @@ class Command(BaseCommand):
 
     def __init__(self):
         super().__init__()
-        self.tg_client = TgClient(TOKEN_TELEGRAM_BOT)
         self.tg_user = TgUser
         self.allow_commands_list = {
             '/goals': self._get_goals,
@@ -36,9 +35,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         offset = 0
-
+        tg_client = TgClient(TOKEN_TELEGRAM_BOT)
         while True:
-            res: GetUpdatesResponse = self.tg_client.get_updates(offset=offset)
+            res: GetUpdatesResponse = tg_client.get_updates(offset=offset)
             for item in res.result:
                 offset = item.update_id + 1
                 chat_id = item.message.chat.id
@@ -46,34 +45,34 @@ class Command(BaseCommand):
                 tg_user: TgUser = self._check_user_existence(username, chat_id)
 
                 if not tg_user.user_id:
-                    self.tg_client.send_message(chat_id=chat_id, text=f'Привет {username}')
+                    tg_client.send_message(chat_id=chat_id, text=f'Привет {username}')
                     verification_code = tg_user.generate_verification_code()
-                    self.tg_client.send_message(chat_id=chat_id, text=f'Подтвердите, пожалуйста, свой аккаунт. '
+                    tg_client.send_message(chat_id=chat_id, text=f'Подтвердите, пожалуйста, свой аккаунт. '
                                                                       f'Для подтверждения необходимо ввести код: '
                                                                       f'{verification_code} '
                                                                       f'на сайте vshtefan.ga')
                 else:
-                    self._get_message_authorized_users(item.message, tg_user)
+                    self._get_message_authorized_users(item.message, tg_user, tg_client)
 
-    def _get_message_authorized_users(self, message: Message, tg_user: TgUser):
+    def _get_message_authorized_users(self, message: Message, tg_user: TgUser, tg_client):
         user_state = redis_instance.get(tg_user.tg_id)
         if message.text == '/cancel':
             redis_instance.delete(tg_user.tg_id)
             redis_instance.delete(f'{tg_user.tg_id}cat_name')
-            self.tg_client.send_message(chat_id=message.chat.id, text='Операция отменена')
+            tg_client.send_message(chat_id=message.chat.id, text='Операция отменена')
             return
         if user_state in self.commands:
-            answer = self.commands[user_state](message=message, tg_user=tg_user)
-            self.tg_client.send_message(chat_id=message.chat.id, text=answer)
+            answer = self.commands[user_state](message=message, tg_user=tg_user, tg_client=tg_client)
+            tg_client.send_message(chat_id=message.chat.id, text=answer)
             return
         if message.text in self.allow_commands_list:
             redis_instance.set(tg_user.tg_id, message.text)
-            answer = self.allow_commands_list[message.text](message, tg_user)
-            self.tg_client.send_message(chat_id=message.chat.id, text=answer)
+            answer = self.allow_commands_list[message.text](message, tg_user, tg_client)
+            tg_client.send_message(chat_id=message.chat.id, text=answer)
             return
-        self.tg_client.send_message(chat_id=message.chat.id, text='неизвестная команда')
+        tg_client.send_message(chat_id=message.chat.id, text='неизвестная команда')
 
-    def _get_goals(self, message: Message, tg_user: TgUser) -> str:
+    def _get_goals(self, message: Message, tg_user: TgUser, tg_client) -> str:
         user_goals: list[Goal] = Goal.objects.prefetch_related('category').filter(
             Q(category__board__participants__user_id=tg_user.user_id) &
             ~Q(status=Goal.Status.archived) &
@@ -83,8 +82,8 @@ class Command(BaseCommand):
         redis_instance.delete(tg_user.tg_id)
         return goals_list if goals_list else 'Активные цели не найдены'
 
-    def _create_category(self, message: Message, tg_user: TgUser):
-        self.tg_client.send_message(chat_id=message.chat.id,
+    def _create_category(self, message: Message, tg_user: TgUser, tg_client):
+        tg_client.send_message(chat_id=message.chat.id,
                                     text='Введите название категории для создания или /cancel для отмены'
                                     )
         user_categories = GoalCategory.objects.prefetch_related('board__participants__user').filter(
@@ -92,10 +91,10 @@ class Command(BaseCommand):
             is_deleted=False
         )
         categories_list = "\n".join(f"#{category.id} {category.title}" for category in user_categories)
-        self.tg_client.send_message(chat_id=message.chat.id, text=categories_list)
+        tg_client.send_message(chat_id=message.chat.id, text=categories_list)
         redis_instance.set(tg_user.tg_id, 'set_name_category')
 
-    def _set_name_category(self, message: Message, tg_user: TgUser):
+    def _set_name_category(self, message: Message, tg_user: TgUser, tg_client):
         goal_category: list[GoalCategory] = GoalCategory.objects.prefetch_related('board__participants__user').filter(
             Q(board__participants__user_id=tg_user.user_id) &
             Q(is_deleted=False) &
@@ -104,19 +103,19 @@ class Command(BaseCommand):
         if goal_category:
             redis_instance.append(f'{tg_user.tg_id}cat_name', goal_category[0].id)
             redis_instance.set(tg_user.tg_id, 'set_name_goal')
-            self.tg_client.send_message(chat_id=message.chat.id,
+            tg_client.send_message(chat_id=message.chat.id,
                                         text='Введите название цели для создания или /cancel для отмены')
             return
-        self.tg_client.send_message(chat_id=message.chat.id, text='Не верное имя категории попробуйте еще раз')
+        tg_client.send_message(chat_id=message.chat.id, text='Не верное имя категории попробуйте еще раз')
 
-    def _set_name_goal(self, message: Message, tg_user: TgUser):
+    def _set_name_goal(self, message: Message, tg_user: TgUser, tg_client):
         obj = Goal.objects.create(user_id=tg_user.user_id,
                                   title=message.text,
                                   category_id=int(redis_instance.get(f'{tg_user.tg_id}cat_name'))
                                   )
         if obj:
-            self.tg_client.send_message(chat_id=message.chat.id, text='Цель успешно создана')
+            tg_client.send_message(chat_id=message.chat.id, text='Цель успешно создана')
             redis_instance.delete(tg_user.tg_id)
             redis_instance.delete(f'{tg_user.tg_id}cat_name')
             return
-        self.tg_client.send_message(chat_id=message.chat.id, text='Что то не так')
+        tg_client.send_message(chat_id=message.chat.id, text='Что то не так')
